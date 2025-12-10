2f91e699b8077e5194a94424e23e67b85aac9953
Atul70911 <atul7091143875@gmail.com>
2025-12-10 16:57:33 +0530

    Add deployment workflow

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -1,8 +1,4 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
 export default defineConfig({
-   base: 'frontendeval-faq',
+  base: '/frontendeval-faq/',  // Your repo name from the screenshot
   plugins: [react()],
 })
